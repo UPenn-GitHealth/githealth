0fd3d1cf2d795d2bc2700353409c11b1038e22a1
Riju Datta <dattarij@seas.upenn.edu>
2023-11-28 19:59:33 -0500

    working version (w/o images)

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 
 // import { useSession } from 'next-auth/client'
 
@@ -13,27 +13,43 @@ export default function Home() {
         </h1>
 
         <div className="grid">
-          <a href="/pulls_per_repo" className="card">
-          " className="card">
+          <br></br>
+          <a href="/get_pulls_per_repo" className="card">
             <h3>Pulls Per Repo &rarr;</h3>
-            <p>Graph of pulls per repo</p>
+            <p>(Graph of pulls per repo)</p>
           </a>
+
+          <br></br>
 
           <a href="/open_pulls_per_repo" className="card">
             <h3>Open Pulls Per Repo &rarr;</h3>
-            <p>Graph of open pulls per repo</p>
+            <p>(Graph of open pulls per repo)</p>
           </a>
+
+          <br></br>
 
           <a href="/pulls_per_month" className="card">
             <h3>Pulls Per Month &rarr;</h3>
-            <p>Graph of pulls per month</p>
+            <p>(Graph of pulls per month)</p>
 
           </a>
+
+          <br></br>
 
           <a href="/rolling_avg_days_to_merge" className="card">
             <h3>Rolling Avg Days to Merge &rarr;</h3>
-            <p>Graph of rolling avg days to merge</p>
+            <p>(Graph of rolling avg days to merge)</p>
           </a>
+
+          <br></br>
+
+
+
+          <br></br>
+
+
+
+
         </div>
       </main>
 
